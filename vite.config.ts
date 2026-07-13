b1642fd7524bd2b96262a8575ab1aeccaed8a36e
Vitaliy <mr.varvshenya.vn@gmail.com>
2026-07-14 02:00:58 +0500

    Обновить конфиг и SEO-адреса под корневой домен tvoe-sobytie.github.io

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/tvoe-sobytie.github.io/',
+    base: '/',
     plugins: [react()],
     server: {
         host: true,           // ��������� ������ � �������� � ������ ��������� � ����
